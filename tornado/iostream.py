d81d99151cddc4e5e2121597ed4dbb7504d42929
bronana <bronan@glance.net>
2019-10-18 17:07:40 -0400

    logging patch 4 glance

--- tornado/iostream.py
+++ tornado/iostream.py
@@ -1523,8 +1523,7 @@ class SSLIOStream(IOStream):
             # to cause do_handshake to raise EBADF and ENOTCONN, so make
             # those errors quiet as well.
             # https://groups.google.com/forum/?fromgroups#!topic/python-tornado/ApucKJat1_0
-            if (self._is_connreset(err) or
-                    err.args[0] in (errno.EBADF, errno.ENOTCONN)):
+            if (self._is_connreset(err) or err.args[0] in (0, errno.EBADF, errno.ENOTCONN)):
                 return self.close(exc_info=err)
             raise
         except AttributeError as err:
